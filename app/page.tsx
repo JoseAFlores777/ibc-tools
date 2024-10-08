5c4d78056cbe35c1140bc02457d5e8faf7713f71
Jose Adolfo Izaguirre <joseadolfoizaguirreflores@gmail.com>
2024-10-07 22:52:59 -0600

    fix: Adjust height in homepage background container
    - Changed the height of a div element from 500px to 1000px in the homepage background container.

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function HomePage() {
 						" src="/images/altar_2.jpg" alt="hero" width={1920} height={1080} />
 				<div className="absolute inset-0 bg-gradient-to-t from-white via-white/60 to-white z-20"></div>
 			</div>
-			<div className="h-[500px] bg-slate-500"></div>
+			<div className="h-[1000px] bg-slate-500"></div>
 		</div>
 	);
 }
