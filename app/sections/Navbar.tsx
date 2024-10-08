ff010dd487b53a7e098adaadccda6845424a7531
Jose Adolfo Izaguirre <joseadolfoizaguirreflores@gmail.com>
2024-10-07 22:53:06 -0600

    feat: Integrate Framer Motion for Navbar Animation
    - Added Framer Motion library for animations
    - Implemented animated opening and closing of NavbarMenu
    - Updated NavbarHeader font style to 'font-normal'

--- app/sections/Navbar.tsx
+++ app/sections/Navbar.tsx
@@ -7,6 +7,7 @@ import { Nav } from "@/lib/shadcn/components";
 import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
 import { useState, useEffect, FC } from "react";
 import { clsx } from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
 import {
   AlertCircle,
   Archive,
@@ -22,7 +23,6 @@ import {
 } from "lucide-react";
 
 export interface NavbarProps {}
-
 const Navbar: FC<NavbarProps> = () => {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
@@ -38,14 +38,31 @@ const Navbar: FC<NavbarProps> = () => {
 
   return (
     <>
-      <div className={clsx("top-0 z-50", isOpen ? "fixed w-[100vw] bg-white/10 backdrop-blur-lg" : "sticky bg-transparent")}>
+      <div
+        className={clsx(
+          'top-0 z-50 h-auto fixed w-[100vw] transition-all duration-500 bg-white/10 backdrop-blur-lg',
+          isOpen ? ' bg-white/10 backdrop-blur-lg' : 'bg-transparent'
+        )}
+      >
         <NavbarHeader isOpen={isOpen} onClick={toggleMenu} />
-        {isOpen && <NavbarMenu />}
+        
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              key="navbarMenu"
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: 'auto', opacity: 1 }}
+              exit={{ display: 'none', height: 0 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <NavbarMenu />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
 };
-
 interface NavbarHeaderProps {
   isOpen: boolean;
   onClick: () => void;
@@ -54,7 +71,7 @@ interface NavbarHeaderProps {
 const NavbarHeader: FC<NavbarHeaderProps> = ({ isOpen, onClick }) => (
   <div className="w-full h-auto p-5 bg-transparent flex justify-between items-center transition duration-500 ease-in-out transform">
     <Image src="/images/IBC_Logo-min.png" alt="logo" width={50} height={50} />
-    <span className="text-lg text-primary font-light">El Calvario</span>
+    <span className="text-lg text-primary font-normal">El Calvario</span>
     <Button onClick={onClick} variant="link" size="icon" className="bg-transparent">
       {isOpen ? <Cross1Icon className="h-7 w-7 text-primary" /> : <HamburgerMenuIcon className="h-7 w-7 text-primary" />}
     </Button>
@@ -65,12 +82,8 @@ const NavbarMenu: FC = () => (
   <div className="h-auto max-h-full overflow-hidden bg-white/10 backdrop-blur-lg transition duration-500 ease-in-out transform">
     <Separator orientation="horizontal" />
     <ScrollArea className="h-svh px-5 pt-5 pb-32">
-      {/* <Nav
-        className="flex flex-col gap-4 pb-[300px]"
-        isCollapsed={false}
-        links={getNavLinks()}
-      /> */}
-          <Accordion type="single" collapsible className="w-full">
+
+      <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
         <AccordionTrigger>Is it accessible?</AccordionTrigger>
         <AccordionContent>
@@ -95,6 +108,13 @@ const NavbarMenu: FC = () => (
           prefer.
         </AccordionContent>
       </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
     </Accordion>
     </ScrollArea>
   </div>
@@ -108,41 +128,6 @@ const getNavLinks = () => [
   { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
   { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
   { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
-  { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
-  { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
-  { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
-  { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
-  { title: "Inbox", label: "128", icon: Inbox, variant: "ghost" as const },
-  { title: "Drafts", label: "9", icon: File, variant: "ghost" as const },
-  { title: "Sent", label: "", icon: Send, variant: "ghost" as const },
-  { title: "Junk", label: "23", icon: ArchiveX, variant: "ghost" as const },
-  { title: "Trash", label: "", icon: Trash2, variant: "ghost" as const },
-  { title: "Archive", label: "", icon: Archive, variant: "ghost" as const },
   // Puedes agregar más links aquí si es necesario
 ];
 
