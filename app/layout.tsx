0c6e1c2c5300a420bcf25713af8f81a00d7c8caa
Jose Adolfo Izaguirre <joseadolfoizaguirreflores@gmail.com>
2025-10-09 00:40:30 -0600

    feat: integrate production analytics script
    Incorporates a third-party analytics script that loads only in the
    production environment to enable website usage tracking. Ensures that
    performance and data collection are not affected during development.
    
    Enhances monitoring and provides insights for continuous improvement.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import { cn } from "@/app/lib/shadcn/utils"
+import { cn } from "@/app/lib/shadcn/utils";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-
 export const metadata: Metadata = {
   title: "IBC | El Calvario",
   description: "Iglesia Bautista El Calvario",
   icons: {
     icon: {
-      url: '/favicon_io/favicon-32x32.png',
-      type: 'image/png',
-    }
-  }
+      url: "/favicon_io/favicon-32x32.png",
+      type: "image/png",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -27,10 +27,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body  className={cn(
+      <head>
+        {process.env.NODE_ENV === "production" && (
+          <Script
+            src="https://analytics.joseiz.com/script.js"
+            data-website-id="097889f1-b030-4066-9a99-29e6e308ac27"
+            strategy="afterInteractive"
+          />
+        )}
+      </head>
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.variable
-        )}>{children}</body>
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
